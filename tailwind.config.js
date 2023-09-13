6b7537b1fbb590f6b200f036e56180c2e3320640
ilham05 <ilhammaulana05303005@gmail.com>
2023-09-13 19:54:03 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}", 
+  "./node_modules/flowbite/**/*.js", 
+  './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [ require('flowbite/plugin')],
+}
